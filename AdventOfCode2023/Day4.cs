7077cf2fae1d1555cd23a77a8af098712eb24a7f
romain <hello@romainquinet.com>
2023-12-25 19:39:16 +0100

    Day 4 part 2

--- AdventOfCode2023/Day4.cs
+++ AdventOfCode2023/Day4.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace AdventOfCode2023;
 
 public class Day4 : Challenge
@@ -24,6 +26,40 @@ public class Day4 : Challenge
 
     public override int Part2(string input)
     {
-        return 0;
+        var lines = input.Split('\n');
+        var cache = new Dictionary<int, int>();
+        var cardCount = 0;
+
+        void ProcessCard(int cardIndex)
+        {
+            var matchCount = 0;
+            if (cache.TryGetValue(cardIndex, out var value))
+            {
+                matchCount = value;
+            }
+            else
+            {
+                var line = lines![cardIndex];
+                var data = line.Split(':')[1];
+                var split = data.Split('|');
+                var winnings = split[0].Split(' ').Where(x => !string.IsNullOrEmpty(x)).Select(int.Parse);
+                var draws = split[1].Split(' ').Where(x => !string.IsNullOrEmpty(x)).Select(int.Parse);
+                matchCount = draws.Intersect(winnings).Count();
+                cache[cardIndex] = matchCount;
+            }
+
+            for (int j = 1; j <= matchCount; j++)
+            {
+                if (cardIndex + j > lines.Length) break;
+                ProcessCard(cardIndex + j);
+            }
+
+            cardCount++;
+        }
+        
+        for (var i = 0; i < lines.Length; i++)
+            ProcessCard(i);
+
+        return cardCount;
     }
 }
